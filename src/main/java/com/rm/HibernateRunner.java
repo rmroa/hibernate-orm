d23be06dede3f556688305d64432d8ba01e1d37c
rm <romtho@mail.ru>
2021-12-01 20:48:57 +0300

    Custom user type

--- src/main/java/com/rm/HibernateRunner.java
+++ src/main/java/com/rm/HibernateRunner.java
@@ -4,6 +4,7 @@ import com.rm.entity.Birthday;
 import com.rm.entity.Gender;
 import com.rm.entity.Role;
 import com.rm.entity.User;
+import com.vladmihalcea.hibernate.type.json.JsonBinaryType;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -27,6 +28,7 @@ public class HibernateRunner {
 //        configuration.addAnnotatedClass(User.class); // аналог <mapping class="com.rm.entity.User"/>
 //        configuration.setPhysicalNamingStrategy(new CamelCaseToUnderscoresNamingStrategy()); // аналог @Column(name = "...")
 //        configuration.addAttributeConverter(new BirthdayConverter(), true); // аналог @Convert(converter = BirthdayConverter.class)
+        configuration.registerTypeOverride(new JsonBinaryType()); // регистрация JsonBinaryType для поля "private String info";
         configuration.configure();
 
         try (SessionFactory sessionFactory = configuration.buildSessionFactory();
@@ -43,6 +45,10 @@ public class HibernateRunner {
                     .phone("375292901210")
                     .email("firstname@mail.ru")
                     .password("password")
+                    .info("{\n" +
+                            "      \"name\": \"FirstName\",\n" +
+                            "      \"id\": 25\n" +
+                            "    }")
                     .role(Role.CUSTOMER)
                     .gender(Gender.MALE)
                     .build();
--- src/main/java/com/rm/entity/User.java
+++ src/main/java/com/rm/entity/User.java
@@ -1,10 +1,13 @@
 package com.rm.entity;
 
 import com.rm.converter.BirthdayConverter;
+import com.vladmihalcea.hibernate.type.json.JsonBinaryType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.TypeDef;
 
 import javax.persistence.Column;
 import javax.persistence.Convert;
@@ -23,6 +26,7 @@ import static javax.persistence.GenerationType.IDENTITY;
 @Builder
 @Entity
 @Table(name = "users")
+@TypeDef(name = "rm", typeClass = JsonBinaryType.class)
 public class User {
 
     @Id
@@ -49,6 +53,11 @@ public class User {
     private String email;
 
     private String password;
+
+    @Type(type = "rm")
+//    @Type(type = "com.vladmihalcea.hibernate.type.json.JsonBinaryType")
+    // можем использовать "jsonb" т.к. метод JsonBinaryType getName() возвращает "jsonb"
+    private String info;
 
     @Enumerated(EnumType.STRING)
     private Role role;
