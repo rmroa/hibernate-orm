4092fbaa664a07869935c895dfec98131f0263ef
rm <romtho@mail.ru>
2022-01-09 22:38:48 +0300

    TestContainers, util, configuration

--- src/main/java/com/rm/util/HibernateUtil.java
+++ src/main/java/com/rm/util/HibernateUtil.java
@@ -8,12 +8,16 @@ import org.hibernate.cfg.Configuration;
 public class HibernateUtil {
 
     public static SessionFactory buildSessionFactory() {
-        Configuration configuration = new Configuration();
+        Configuration configuration = buildConfiguration();
+        configuration.configure();
+        return configuration.buildSessionFactory();
+    }
+
+    public static Configuration buildConfiguration() {
 //        configuration.addAnnotatedClass(User.class); // аналог <mapping class="com.rm.entity.User"/>
 //        configuration.setPhysicalNamingStrategy(new CamelCaseToUnderscoresNamingStrategy()); // аналог @Column(name = "...")
 //        configuration.addAttributeConverter(new BirthdayConverter(), true); // аналог @Convert(converter = BirthdayConverter.class)
 //        configuration.registerTypeOverride(new JsonBinaryType()); // регистрация JsonBinaryType для поля "private String info";
-        configuration.configure();
-        return configuration.buildSessionFactory();
+        return new Configuration();
     }
 }
