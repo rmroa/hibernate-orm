1dc0c72cf2fa8f2ed72977bcd89b5c7371ac4c7d
rm <romtho@mail.ru>
2022-02-17 20:46:08 +0300

    Locks Pessimistic

--- src/main/java/com/rm/HibernateRunner.java
+++ src/main/java/com/rm/HibernateRunner.java
@@ -6,6 +6,7 @@ import com.rm.util.TestDataImporter;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
+import javax.persistence.LockModeType;
 import javax.transaction.Transactional;
 import java.math.BigDecimal;
 import java.sql.SQLException;
@@ -21,10 +22,15 @@ public class HibernateRunner {
             session.beginTransaction();
             session1.beginTransaction();
 
+//            session.createQuery("select m from Model m", Model.class)
+//                    .setLockMode(LockModeType.PESSIMISTIC_READ)
+//                    .setHint("javax.persistence.lock.timeout", 5000)
+//                    .list();
+
             TestDataImporter.importData(sessionFactory);
 
-            Model model = session.find(Model.class, 1L);
-            model.setPrice(new BigDecimal("100.100"));
+            Model model = session.find(Model.class, 1L, LockModeType.PESSIMISTIC_FORCE_INCREMENT);
+            model.setPrice(new BigDecimal("1090.100"));
 
             Model model1 = session1.find(Model.class, 1L);
             model1.setPrice(new BigDecimal("100.100"));
--- src/main/java/com/rm/entity/Model.java
+++ src/main/java/com/rm/entity/Model.java
@@ -6,11 +6,8 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
-import org.hibernate.annotations.DynamicUpdate;
 import org.hibernate.annotations.FetchMode;
 import org.hibernate.annotations.FetchProfile;
-import org.hibernate.annotations.OptimisticLockType;
-import org.hibernate.annotations.OptimisticLocking;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
@@ -27,6 +24,7 @@ import javax.persistence.NamedEntityGraph;
 import javax.persistence.NamedSubgraph;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.persistence.Version;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -58,17 +56,14 @@ import static javax.persistence.GenerationType.IDENTITY;
 @Builder
 @Entity
 @Table(name = "models")
-@OptimisticLocking(type = OptimisticLockType.ALL)
-//@OptimisticLocking(type = OptimisticLockType.DIRTY)
-@DynamicUpdate
 public class Model {
 
     @Id
     @GeneratedValue(strategy = IDENTITY)
     private Long id;
 
-//    @Version
-//    private Long version;
+    @Version
+    private Long version;
 
     private String model;
 
