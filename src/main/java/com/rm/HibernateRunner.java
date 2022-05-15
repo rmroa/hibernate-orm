df508d668ca8f549974b6d40971acea2df7c3924
rm <romtho@mail.ru>
2022-05-15 22:29:59 +0300

    Second Level Cache. Configuration

--- src/main/java/com/rm/HibernateRunner.java
+++ src/main/java/com/rm/HibernateRunner.java
@@ -2,14 +2,10 @@ package com.rm;
 
 import com.rm.entity.Model;
 import com.rm.util.HibernateUtil;
-import org.hibernate.ReplicationMode;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.envers.AuditReader;
-import org.hibernate.envers.AuditReaderFactory;
 
 import javax.transaction.Transactional;
-import java.sql.Date;
 import java.sql.SQLException;
 
 public class HibernateRunner {
@@ -25,18 +21,6 @@ public class HibernateRunner {
                 Model model = session.find(Model.class, 1L);
 
                 session.getTransaction().commit();
-            }
-
-            try (Session session2 = sessionFactory.openSession()) {
-                session2.beginTransaction();
-
-                AuditReader auditReader = AuditReaderFactory.get(session2);
-                Model oldModel = auditReader.find(Model.class, 1L, new Date(1652298315674L));
-                session2.replicate(oldModel, ReplicationMode.OVERWRITE);
-
-                System.out.println();
-
-                session2.getTransaction().commit();
             }
         }
     }
